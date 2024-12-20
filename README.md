4068022baa61c1ffa687ed8517216080074b79a3
Rafael Vera Moreno <79293099+rafaelsteven@users.noreply.github.com>
2024-12-19 23:35:22 -0500

    challenge_16

--- README.md
+++ README.md
@@ -23,6 +23,7 @@ Mis soluciones para los [24 días de retos con JavaScript](https://adventjs.dev/
 | 13  | [Reto #13: 🤖 ¿El robot está de vuelta?](https://adventjs.dev/es/challenges/2024/13) | [solución](./challenges/challenge_13/index.js) | <img src="./img/13.png" width="50" height="50" /> | ✨✨✨✨✨    |
 | 14  | [Reto #14: 🦌 Acomodando los renos](https://adventjs.dev/es/challenges/2024/14) | [solución](./challenges/challenge_14/index.js) | <img src="./img/14.png" width="50" height="50" /> | ✨✨✨✨✨    |
 | 15  | [Reto #15: ✏️ Dibujando tablas](https://adventjs.dev/es/challenges/2024/15) | [solución](./challenges/challenge_15/index.js) | <img src="./img/15.png" width="50" height="50" /> | ✨✨✨✨    |
+| 16  | [Reto #16: ❄️ Limpiando la nieve del camino](https://adventjs.dev/es/challenges/2024/16) | [solución](./challenges/challenge_16/index.js) | <img src="./img/16.png" width="50" height="50" /> | ✨✨✨✨✨   |
 
 ## Como ejecutar el código :running:
 
---
+++ challenges/challenge_16/index.js
@@ -0,0 +1,42 @@
+/*
+
+Los elfos están trabajando arduamente para limpiar los caminos llenos de nieve mágica ❄️. Esta nieve tiene una propiedad especial: si dos montículos de nieve idénticos y adyacentes se encuentran, desaparecen automáticamente.
+
+Tu tarea es escribir una función que ayude a los elfos a simular este proceso. El camino se representa por una cadena de texto y cada montículo de nieve un carácter.
+
+Tienes que eliminar todos los montículos de nieve adyacentes que sean iguales hasta que no queden más movimientos posibles.
+
+El resultado debe ser el camino final después de haber eliminado todos los montículos duplicados:
+
+removeSnow('zxxzoz') // -> "oz"
+// 1. Eliminamos "xx", quedando "zzoz"
+// 2. Eliminamos "zz", quedando "oz"
+
+removeSnow('abcdd') // -> "abc"
+// 1. Eliminamos "dd", quedando "abc"
+
+removeSnow('zzz') // -> "z"
+// 1. Eliminamos "zz", quedando "z"
+
+removeSnow('a') // -> "a"
+// No hay montículos repetidos
+
+*/
+
+
+/**
+ * @param {string} s
+ * @returns {string}
+ */
+//5 estrellas
+function removeSnow(s) {
+    // Code here
+    let stack = [];
+    for( const chat of s){
+        stack = stack.includes(chat)
+                ? stack.filter(item => item !== chat) // Elimina si existe
+                : [...stack, chat];
+    }
+    return stack.join('');
+  }
+console.log(removeSnow('zzz'));
